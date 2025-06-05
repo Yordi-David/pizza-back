e891d109c18ba2c1f067d60ea47407f1dbd05ca5
y.david <p63132@nrb.be>
2025-06-05 15:26:43 +0200

    config ConfigModule & TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfig, DbConfig } from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			cache: true,
+			load: [AppConfig, DbConfig]
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				...configService.get('database')
+			}),
+			inject: [ConfigService]
+		})
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/database.config.ts
@@ -0,0 +1,15 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+    type: 'mssql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_SERVER_PORT!) || 'localhost',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [`${__dirname}/../**/*.entity.ts`],
+    synchronize: process.env.NODE_ENV === 'development',
+    logging: process.env.NODE_ENV === 'development',
+    migrations: [`${__dirname}/../..db/migrations/*{.ts,.js}`],
+    migrationsTableName: process.env.DB_MIGRATION_TABLE
+}));
